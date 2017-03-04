aee8321ab99063263b57026aa00522d5605ce19c
fznply <fznply@163.com>
2017-03-04 19:04:48 +0800

    make .rsync_save.sh template better

--- .rsync_save.sh
+++ .rsync_save.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-src="srcDir"
+cd `dirname $0`
+
+src="./srcDir"
 dst="loginUser@remoteServer:dstDir"
-srcExclude=""
+srcExclude=".rsync_save.sh"
 dstExclude=""
 rsync -avz --exclude $srcExclude --delete --exclude $dstExclude $src $dst
+
 exit 0
